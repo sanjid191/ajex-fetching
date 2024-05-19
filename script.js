28557d57dbb5f3006c5e219eb690322c26bf10a6
Sanjid Ahmmed <sanjid.sa191@gmail.com>
2024-05-19 20:14:37 +0600

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('fetchRepo').addEventListener('click', function() {
 });
 
 function fetchDemoData(repoName) {
-    const url = `https://raw.githubusercontent.com/sanjid191/ajex-fetching/main/demoData.json`;
+    const url = `https://git@github.com:sanjid191/ajex-fetching.gitn`;
 
     fetch(url)
         .then(response => {
